a83d0da8ea9ed78afc4b635da990e3e8692f8404
Emilian Stankov <emilian.stankov@bulpros.com>
2017-04-09 19:40:00 +0300

    Implement Logger

--- ShipGame/Logger.cs
+++ ShipGame/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,38 @@ namespace ShipGame
 {
     class Logger
     {
+        private static List<string> messages = new List<string>();
+        private static int startingLine = 0;
+        private static int LOG_LENGTH = 5;
+
+        public static void Write(string message)
+        {
+            messages.Add(message);
+            if(startingLine == 0)
+            {
+                startingLine = Console.CursorTop;
+            }
+            if (Console.CursorTop == startingLine + LOG_LENGTH)
+            {
+                CleaLog();
+            }
+            Console.WriteLine(message);
+        }
+
+        private static void CleaLog()
+        {
+            for (int i = 0; i < LOG_LENGTH; i++)
+            {
+                Console.SetCursorPosition(0, startingLine + i);
+                Console.Write("\r" + new string(' ', Console.BufferWidth) + "\r");
+                Console.SetCursorPosition(0, startingLine);
+            }
+        }
+
+        public static void SaveToFile()
+        {
+            string path = "GameLog_" + DateTime.Now.ToString("yyyy.MM.dd_HH.mm.ss") + ".txt";
+            File.WriteAllLines(path, messages.ToArray());
+        }
     }
 }
